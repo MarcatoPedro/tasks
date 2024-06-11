71bb478d1a8861eb454a59ed5839c20d0d676a7c
unknown <pedrootaviomarcato@gmail.com>
2024-06-11 10:39:10 -0300

    Criação do TaskList2

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react"
-import {StyleSheet, View, Text} from "react-native"
+import {StyleSheet, View, Text, ImageBackground} from "react-native"
+import moment from "moment"
+import "moment/locale/pt-br "
+
+import today_image from "../../assets/imgs/today.jpg"
 
 export default class TaskList extends Component{
     //função do Component que vai mostrar uma tela
     render(){
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return(
             <View style = {styles.container}>
-                <Text>Lista de Tarefas</Text>
+
+                <ImageBackground source={today_image} style={styles.background}>
+                    <View style={styles.titleBar}>
+                        <Text style={styles.title}>Hoje</Text>
+                        <Text style={styles.subTitle}>{today}</Text>
+                    </View>
+                </ImageBackground>
+
+                <View style={styles.taskList}>
+                    <Text>Lista de Tarefas</Text>
+                </View>
             </View>
         )
     }
@@ -15,5 +30,29 @@ export default class TaskList extends Component{
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-    }
+    },
+    background:{
+         flex: 3,
+    },
+    taskList:{
+         flex: 7,
+    },
+    titleBar:{
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    title:{
+        fontFamily: 'Arial',
+        fontSize: 50,
+        color: '#FFF',
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subTitle:{
+        fontFamily: 'Arial',
+        fontSize: 20,
+        color: '#FFF',
+        marginLeft: 20,
+        marginBottom: 30,
+    },
 })
